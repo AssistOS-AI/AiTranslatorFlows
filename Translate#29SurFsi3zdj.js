363e13a162cfcc1e12f363955b42e656661b4525
alupoaiemircea <mircea@axiologic.net>
2024-03-27 11:10:07 +0200

    fix #44

--- Translate#29SurFsi3zdj.js
+++ Translate#29SurFsi3zdj.js
@@ -1,15 +1,10 @@
 export class Translate {
     static id = "29SurFsi3zdj";
     static description = "Translates a text using a given personality";
-    constructor() {
-
-    }
-
     async start(context, personality) {
         this.prompt = `Please translate the following text: "${context.text}" in the following language: "${context.language}". ${context.prompt || ""}. You will play the role of this personality: "${personality.name}", which has the following characteristics: "${personality.description}". You will translate the given text in the style of the given personality. Return only the translated text.`;
         await this.execute(context.maxTokens);
     }
-
     async execute(maxTokens) {
         let translatedText = await this.request(this.prompt, maxTokens);
         this.return(translatedText);
